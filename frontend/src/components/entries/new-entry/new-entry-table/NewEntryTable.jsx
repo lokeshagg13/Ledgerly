9d00980e41c6e1467060e0b3019b2a9c38c53ae7
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-08-24 00:18:44 +0530

    [UPDATE] - Adding error if fetched balance result into error

--- frontend/src/components/entries/new-entry/new-entry-table/NewEntryTable.jsx
+++ frontend/src/components/entries/new-entry/new-entry-table/NewEntryTable.jsx
@@ -1,14 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { toast } from "react-toastify";
+
 import ContextMenuContext from "../../../../store/context/contextMenuContext";
 import NewEntryRowContextMenu from "./new-entry-row-context-menu/NewEntryRowContextMenu";
 import NewEntryContext from "../../../../store/context/newEntryContext";
 import NewEntryTableRow from "./new-entry-table-row/NewEntryTableRow";
 import NewEntryTableFooter from "./new-entry-table-footer/NewEntryTableFooter";
+import FirmDashboardContext from "../../../../store/context/firmDashboardContext";
+import ErrorImage from "../../../../images/chart-error.png";
 
 function NewEntryTable() {
   const { entryDataRows, menuPos, clickedRow, handleKeyPress } =
     useContext(NewEntryContext);
+  const { overallBalanceError } = useContext(FirmDashboardContext);
   const { checkIfContextMenuVisible } = useContext(ContextMenuContext);
+
+  useEffect(() => {
+    if (overallBalanceError) {
+      toast.error(overallBalanceError, {
+        position: "top-center",
+        autoClose: 10000,
+      });
+      return;
+    }
+  }, [overallBalanceError]);
+
+  if (overallBalanceError) {
+    return (
+      <div className="balance-error">
+        <img src={ErrorImage} alt="" width={150} height="auto" />
+        <p>{overallBalanceError}</p>
+      </div>
+    );
+  }
 
   return (
     <div
