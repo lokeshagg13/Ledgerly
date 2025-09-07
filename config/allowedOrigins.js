172745b12bfa06acccd72e9040c83e06b10f4379
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-09-07 23:52:31 +0530

    [UPDATE] - added allowed origin here

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,5 +3,7 @@ module.exports = [
     "http://localhost:9000",
     "http://192.168.1.11:3000",
     "http://192.168.1.11:9000",
+    "http://192.168.1.7:3000",
+    "http://192.168.1.7:9000",
     "https://lokeshagg13.github.io"
 ];
