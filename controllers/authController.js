0509dab7312a7d0de37a34c7d20b08846727127f
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-09-08 01:56:03 +0530

    [UPDATE] - add domain again

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,12 +68,19 @@ exports.loginUser = async (req, res) => {
     console.log(process.env.NODE_ENV === "production" ? "None" : "Lax")
     console.log(process.env.NODE_ENV === "production")
 
-    res.cookie("jwt", refreshToken, {
+    const cookieOptions = {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000
-    });
+    };
+
+    // only add domain in production
+    if (process.env.NODE_ENV === "production") {
+      cookieOptions.domain = process.env.DOMAIN_NAME;
+    }
+
+    res.cookie("jwt", refreshToken, cookieOptions);
 
     // ✅ Send accessToken + user name + email
     res.status(200).json({
