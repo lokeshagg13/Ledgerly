0bbcbe7d66615d546db217d03abee7ec3ff67e4f
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-09-07 13:26:57 +0530

    [UPDATE] - updated origins to add frontend github pages url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,6 @@ module.exports = [
     "http://localhost:3000",
     "http://localhost:9000",
     "http://192.168.1.11:3000",
-    "http://192.168.1.11:9000"
+    "http://192.168.1.11:9000",
+    "https://lokeshagg13.github.io"
 ];
