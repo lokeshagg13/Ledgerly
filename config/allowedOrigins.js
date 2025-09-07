5de6be0312a877329945bdd9cbe9bd092a18837c
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-09-08 02:20:22 +0530

    [DEPLOY] - deploy with bundled frontend backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,5 +6,6 @@ module.exports = [
     "http://192.168.1.7:3000",
     "http://192.168.1.7:9000",
     "https://lokeshagg13.github.io",
-    "https://ledgerly-app.onrender.com"
+    "https://ledgerly-app.onrender.com",
+    "https://ledgerly-y6sp.onrender.com"
 ];
